b1c9e939280c2280cfcbb1e8f7bb41c6ad2bfc9a
World <48672862+mcsim415@users.noreply.github.com>
2022-01-20 17:03:38 +0900

    Update repair.py

--- repair.py
+++ repair.py
@@ -1,7 +1,51 @@
 import os
 import time
+import threading
 
 tag = '[SCE-TTS-REPAIR] '
+
+class Worker(threading.Thread):
+	"""docstring for Worker"""
+	def __init__(self, folder):
+		super().__init__()
+		self.folder = folder
+
+	def run(self):
+		abs_folder = os.path.abspath(self.folder)
+		current = os.path.join(abs_folder, f'{self.folder}-current.txt')
+		metadata = os.path.join(abs_folder, f'{self.folder}-metadata.txt')
+
+		while True:
+			while not os.path.exists(metadata):
+   				time.sleep(2)
+			if os.path.exists(metadata):
+				log("A new metadata file has been founded.")
+	
+				i = 1
+				f = open(current, "a+")
+				f.seek(0)
+				if os.path.getsize(current) > 0:
+					i = f.read()
+					log("Loaded current state file data.")
+				else:
+					log("Cureent state file is empty. Make a new file.")
+	
+				new_metadata = f'{self.folder}-metadata_{str(i)}.txt'
+				new_metadata_file = os.path.join(abs_folder, new_metadata)
+	
+				try:
+					os.rename(metadata, new_metadata_file)
+				except FileExistsError as e:
+					log("FileExistsError occured. Skipped", "Error")
+				else:
+					log(f"Renamed to `{new_metadata}`. Repaired Succesfully!")
+				finally:
+					f.seek(0)
+					f.truncate()
+					f.write(str(int(i) + 1))
+					f.close()
+					log("Saved current state file data.")
+		
 
 def log(txt, level="Info"):
 	print('['+str(level)+'] '+tag+str(txt))
@@ -30,40 +74,12 @@ folder_list = os.listdir('.')
 if (len(folder_list) - 1) == 0:
 	log("The path could not be found.", "Warning")
 
-while True:
-	for folder in folder_list:
-		if folder == os.path.basename(__file__):
-			continue
+for folder in folder_list:
+	if folder == os.path.basename(__file__):
+		continue
 
-		abs_folder = os.path.abspath(folder)
-		current = os.path.join(abs_folder, f'{folder}-current.txt')
-		metadata = os.path.join(abs_folder, f'{folder}-metadata.txt')
+	log(f"Thread for folder `{folder}` was started.")
+	t = Worker(folder)
+	t.start()
 
-		if os.path.exists(metadata):
-			log("A new metadata file has been founded.")
-			time.sleep(2)
-
-			i = 1
-			f = open(current, "a+")
-			f.seek(0)
-			if os.path.getsize(current) > 0:
-				i = f.read()
-				log("Loaded current state file data.")
-			else:
-				log("Cureent state file is empty. Make a new file.")
-
-			new_metadata = f'{folder}-metadata_{str(i)}.txt'
-			new_metadata_file = os.path.join(abs_folder, new_metadata)
-
-			try:
-				os.rename(metadata, new_metadata_file)
-			except FileExistsError as e:
-				log("FileExistsError occured. Skipped", "Error")
-			else:
-				log(f"Renamed to `{new_metadata}`. Repaired Succesfully!")
-			finally:
-				f.seek(0)
-				f.truncate()
-				f.write(str(int(i) + 1))
-				f.close()
-				log("Saved current state file data.")
+log("All Thread was started.")
